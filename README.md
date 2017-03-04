697fa7a047b43fad5faa63bc2d66be4aa57a8145
Sébastien Mouret <sebastienmouret@Sebastiens-MacBook-Pro.local>
2017-03-04 15:55:43 +0100

    première version

---
+++ README.md
@@ -0,0 +1,20 @@
+PapaGeek Theme pour Ghost
+=========================
+
+Basé sur le thème Bastard.
+
+# Installation
+Mettre le projet dans le dossier
+- ghost/content/themes
+
+Lancer Ghost:
+- npm start
+
+Installation des dépendances :
+- cd ghost/content/themes/PapaGeek
+- npm install
+- gulp watch
+
+Livrer une version
+- gulp bump
+- gulp zip
---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Plugins
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var svgmin = require('gulp-svgmin');
+var header = require('gulp-header');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var cache = require('gulp-cache');
+var runsequence = require('run-sequence');
+var del = require('del');
+var zip = require('gulp-zip');
+var bump = require('gulp-bump');
+
+
+var pkg = require('./package.json');
+var banner = [
+  '/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @homepage <%= pkg.homepage %>',
+  ' * @copyright ' + (new Date()).getFullYear() + ' <%= pkg.author.name %> ' ,
+  ' * @license <%= pkg.license %>',
+  ' */',
+  '\n'
+].join('\n');
+
+var files = {
+    lint: ['gulpfile.js',
+          'package.json',
+          'assets/js/bastard.js'
+          ],
+    sass: ['assets/sass/**/style.scss'],
+    sassall: ['assets/sass/**/*.scss'],
+    css:  ['assets/bower_components/highlightjs/styles/railscasts.css'
+          ],
+    js:   ['assets/bower_components/bootstrap/dist/js/bootstrap.js',
+           'assets/bower_components/highlightjs/highlight.pack.js',
+           'assets/vendor/js/jquery.fitvids.js',
+           'assets/vendor/js/jquery.parallax-1.1.3.js',
+           'assets/vendor/js/jquery.easing.1.3.js',
+           'assets/js/bastard.js'
+           ],
+    images:['image_sources/{,*/}*.jpg',
+            'image_sources/{,*/}*.png'
+           ],
+    svgs:  ['image_sources/{,*/}*.svg',
+           ],
+    clean: [
+            'assets/css/bastard.css',
+            'assets/css/bastard.min.css',
+            'assets/js/scripts.js',
+            'assets/js/scripts.min.js',
+            'assets/js/concat_scripts.js'
+           ]
+};
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src(files.lint)
+         .pipe(jshint())
+         .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('compileSass', function() {
+    return gulp.src(files.sass)
+        .pipe(sass({style: 'expanded', quiet: true, cacheLocation: '.sass-cache'}))
+        .pipe(sass())
+        .pipe(autoprefixer('last 1 version'))
+        .pipe(minifycss({keepSpecialComments: 0}))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('css', function() {
+    runsequence(
+        'compileSass');
+});
+
+// Concatenate & Minify JS
+gulp.task('js', function() {
+    return gulp.src(files.js)
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({
+          suffix: '.min'
+        }))
+        .pipe(header(banner, { pkg : pkg }))
+        .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('concatJs', function() {
+    return gulp.src(files.js)
+        .pipe(concat('concat_scripts.js'))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(header(banner, { pkg : pkg }))
+        .pipe(gulp.dest('assets/js'));
+});
+
+// Images
+gulp.task('imgmin', function() {
+    return gulp.src(files.images)
+        .pipe(cache(imagemin({
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true
+        })))
+        .pipe(gulp.dest('assets/images'));
+});
+
+// Images
+gulp.task('svgmin', function() {
+    return gulp.src(files.svgs)
+        .pipe(cache(svgmin()))
+        .pipe(gulp.dest('assets/images'));
+});
+
+// Build
+gulp.task('build', function() {
+    return gulp.start(['compileSass','concatJs']);
+});
+
+// Watch Files For Changes
+gulp.task('watch', ['default'], function() {
+    gulp.watch(files.lint, ['js']);
+    gulp.watch(files.sassall, ['css']);
+});
+
+// Clean
+gulp.task('clean', function() {
+    files.clean.map(function (x) {
+        console.log("Deleting "+ x);
+      });
+    return del(files.clean);
+});
+
+// Default Task
+gulp.task('default', function() {
+    gulp.start('lint', 'css', 'js', 'imgmin', 'svgmin');
+});
+
+gulp.task('zip', ['build'], function () {
+    return gulp.src(['./**', '!node_modules/**'])
+        .pipe(zip('papageek.zip'))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('bump', function(){
+    gulp.src('./package.json')
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+});
